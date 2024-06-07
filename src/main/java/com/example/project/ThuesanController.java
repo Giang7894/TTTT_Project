6f95ff1b7f90d56adaf8cf020b83dc11e947de4a
GinG1602 <nhnnguyen162002@gmail.com>
2024-06-07 09:04:34 +0700

    abc

--- src/main/java/com/example/project/ThuesanController.java
+++ src/main/java/com/example/project/ThuesanController.java
@@ -1,4 +1,11 @@
 package com.example.project;
 
+import javafx.fxml.FXML;
+import org.w3c.dom.events.MouseEvent;
+
 public class ThuesanController {
+    @FXML
+    void getDat(MouseEvent event) {
+
+    }
 }
